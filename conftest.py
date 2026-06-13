6261f673faa2cfd8fe085cef6397d5a94c18db10
AKSHAY KHATRI <146158556+Akshaykhatri777@users.noreply.github.com>
2026-06-13 12:10:54 +0530

    Refactor setup_and_teardown and api_client fixtures

--- conftest.py
+++ conftest.py
@@ -1,17 +1,109 @@
+# import pytest
+# from selenium import webdriver
+# from config.env import ConfigReader
+# from api.auth_api import AuthAPI
+# from api.notes_api import NotesAPI
+# from pages.login_page import LoginPage
+# from selenium.webdriver.common.by import By
+
+
+# @pytest.fixture(scope="function")
+# def setup_and_teardown():
+#     base_url = ConfigReader.read_config()["qa"]["base_url"]
+
+#     driver = webdriver.Chrome()
+#     driver.maximize_window()
+#     driver.get(base_url)
+
+#     yield driver
+
+#     driver.quit()
+
+
+# @pytest.fixture(scope="session")
+# def api_client():
+
+#     config = ConfigReader.read_config()["qa"]
+#     api_url = ConfigReader.read_config()["qa"]["api_base_url"]
+
+#     auth_api = AuthAPI(api_url)
+
+#     login_response = auth_api.login(
+#         config["username"],
+#         config["passwd"]
+#     )
+
+#     assert login_response.status_code == 200, "API Login Failed"
+
+#     token = login_response.json()["data"]["token"]
+
+#     return {
+#         "token": token,
+#         "notes_api": NotesAPI(config["api_base_url"])
+#     }
+
+
+# import pytest
+# from selenium import webdriver
+# from config.env import ConfigReader
+# from api.auth_api import AuthAPI
+# from api.notes_api import NotesAPI
+# from pages.login_page import LoginPage
+# from selenium.webdriver.common.by import By
+
+
+# @pytest.fixture(scope="function")
+# def setup_and_teardown():
+#     base_url = ConfigReader.read_config()["qa"]["base_url"]
+
+#     driver = webdriver.Chrome()
+#     driver.maximize_window()
+#     driver.get(base_url)
+
+#     yield driver
+
+#     driver.quit()
+
+
+# @pytest.fixture(scope="session")
+# def api_client():
+
+#     config = ConfigReader.read_config()["qa"]
+#     api_url = ConfigReader.read_config()["qa"]["api_base_url"]
+
+#     auth_api = AuthAPI(api_url)
+
+#     login_response = auth_api.login(
+#         config["username"],
+#         config["passwd"]
+#     )
+
+#     assert login_response.status_code == 200, "API Login Failed"
+
+#     token = login_response.json()["data"]["token"]
+
+#     return {
+#         "token": token,
+#         "notes_api": NotesAPI(config["api_base_url"])
+#     }
+
 import pytest
 from selenium import webdriver
 from config.env import ConfigReader
 from api.auth_api import AuthAPI
 from api.notes_api import NotesAPI
-from pages.login_page import LoginPage
-from selenium.webdriver.common.by import By
 
 
 @pytest.fixture(scope="function")
 def setup_and_teardown():
     base_url = ConfigReader.read_config()["qa"]["base_url"]
 
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-notifications")
+
+    driver = webdriver.Chrome(options=options)
+
     driver.maximize_window()
     driver.get(base_url)
 
@@ -24,7 +116,7 @@ def setup_and_teardown():
 def api_client():
 
     config = ConfigReader.read_config()["qa"]
-    api_url = ConfigReader.read_config()["qa"]["api_base_url"]
+    api_url = config["api_base_url"]
 
     auth_api = AuthAPI(api_url)
 
@@ -39,5 +131,5 @@ def api_client():
 
     return {
         "token": token,
-        "notes_api": NotesAPI(config["api_base_url"])
+        "notes_api": NotesAPI(api_url)
     }
